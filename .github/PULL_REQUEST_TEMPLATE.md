25a55f3e607e0b7cef6799f7d914338f0b17a3bb
EXZO <94203269+topdefi@users.noreply.github.com>
2023-03-22 09:36:07 -0700

    Remove source

--- .github/PULL_REQUEST_TEMPLATE.md
+++
@@ -1,3 +0,0 @@
-# Exzo Explorer
-
-Please make changes
--- src/validators/pubkey.ts
+++
@@ -1,8 +0,0 @@
-import { coerce, instance, string } from "superstruct";
-import { PublicKey } from "@velas/web3";
-
-export const PublicKeyFromString = coerce(
-  instance(PublicKey),
-  string(),
-  (value) => new PublicKey(value)
-);
